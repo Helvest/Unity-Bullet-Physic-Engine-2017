613012cf5ebe9229cf24f0b848f7e138d33271c3
Helvest <b.w.f@outlook.com>
2019-01-25 17:29:44 +0100

    up gitignore

---
+++ Assets/BulletUnity/Scripts/Debug/BDebug.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace BulletUnity.Debugging
+{
+	public static class BDebug
+	{
+		[Flags]
+		public enum DebugType
+		{
+			Error = 1,
+			Warning = 2,
+			Debug = 8,
+			Info = 4,
+			Trace = 16,
+		}
+
+		public static void Log(DebugType debugType, object message)
+		{
+			if (EnumExtensions.IsFlagSet(DebugType.Info, debugType))
+			{
+				Debug.Log(message);
+			}
+		}
+
+		public static void LogWarning(DebugType debugType, object message)
+		{
+			if (EnumExtensions.IsFlagSet(DebugType.Warning, debugType))
+			{
+				Debug.LogWarning(message);
+			}
+		}
+
+		public static void Log(DebugType debugType, object message, params object[] arguments)
+		{
+			if (EnumExtensions.IsFlagSet(DebugType.Info, debugType))
+			{
+				Debug.Log(string.Format(message.ToString(), arguments));
+			}
+		}
+
+		public static void LogWarning(DebugType debugType, object message, params object[] arguments)
+		{
+			if (EnumExtensions.IsFlagSet(DebugType.Warning, debugType))
+			{
+				Debug.LogWarning(string.Format(message.ToString(), arguments));
+			}
+		}
+	}
+}
